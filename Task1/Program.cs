57de209bb08328dadcd108e513048d2fbd334ae3
Maxim <lyskomax2@mail.ru>
2024-01-09 19:23:36 +0300

    Add info Task1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Создание массива из 5
+int[] arr = new int[5];
+arr[0] = 5;
+arr[1] = 7;
+arr[2] = 5;
+arr[3] = 4;
+arr[4] = 3;
+Console.WriteLine(arr[0]);
+Console.WriteLine(arr[1]);
+Console.WriteLine(arr[2]);
+Console.WriteLine(arr[3]);
+Console.WriteLine(arr[4]);
+
+// Недопустимое обращение, индекс выходит за границы массива
+// Console.WriteLine(arr[5]);
+
+// Создание массива без отдельного ввода каждого его элемента
+int[] arr2 = new int[5] { 1, 2, 3, 4, 5 };
+
+// Упрощенное создание массива (выделение памяти под массив без new int[5])
+int[] arr3 = { 1, 2, 3, 4, 5 };
+
+// Создание массива на pyhton 
+// arr = [1,2,3,4,5]
