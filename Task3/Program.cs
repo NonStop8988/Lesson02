3155d9f4533620450f5abde92e95361948790c5d
Maxim <lyskomax2@mail.ru>
2024-01-09 23:05:40 +0300

    Add Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,12 @@
+﻿int[] arr = { 1, 4, 6, 7, 2, 5, 8, 9, 4, 6 };
+int i = 0;
+
+while (i < arr.Length)
+{
+    if (arr[i] % 2 == 0)
+    {
+        Console.Write($"{arr[i]}; ");
+    }
+    i += 1;
+}
+    
