5951755bae6cc9adbd9d9974890df51cf9e6a9fe
Maxim <lyskomax2@mail.ru>
2024-01-09 22:32:08 +0300

    Отредактировал Task2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,11 +1,16 @@
 ﻿// Создание массива от 1 до заданного числа n
 int n = 10;
-int[] arr = new int[n];
+int[] arr = new int[n]; // new int[n] - выделение памяти под новый массив
 int i = 0;
 
-while (i < arr.Length)
+while (i < arr.Length) // arr.Length - длина массива
 {
     arr[i] = i + 1;
-    Console.Write($"{arr[i]} ");
+    Console.Write(arr[i]);
     i += 1;
+
+    if (i < arr.Length)
+    {
+        Console.Write($"; ");
+    }
 }
