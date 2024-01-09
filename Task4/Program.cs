84943ccf3b2044b67f0d8fc9bf0b005272ab577c
Maxim <lyskomax2@mail.ru>
2024-01-10 00:19:59 +0300

    Add 3 options cycle

---
+++ Task4/Program.cs
@@ -0,0 +1,33 @@
+﻿int[] arr = { 0, 45, 5, -234, 64, 56, 5 };
+int i = 0;
+int max = arr[0];
+
+while (i < arr.Length)
+{
+    if (max < arr[i])
+    {
+        max = arr[i];
+    }
+    i += 1;
+}
+Console.WriteLine(max);
+
+max = arr[0];
+for (int j = 0; j < arr.Length; j++)
+{
+    if (max < arr[j])
+    {
+        max = arr[j];
+    }
+}
+Console.WriteLine(max);
+
+max = arr[0];
+foreach (int e in arr)
+{
+    if (max < e)
+    {
+        max = e;
+    }
+}
+Console.WriteLine(max);
